767aa08d9b56b68f754ff4f03c7cede17c0201e8
David Esposito <david.esposito@bettercloud.com>
2017-06-02 22:35:17 -0400

    Converting to avro
    Signed-off-by: David Esposito <david.esposito@bettercloud.com>

--- src/main/java/com/example/kafka/tracker/TrackerToKafka.java
+++ src/main/java/com/example/kafka/tracker/TrackerToKafka.java
@@ -1,8 +1,10 @@
 package com.example.kafka.tracker;
 
+import com.example.domain.tracker.AvroCarActivity;
+import com.example.domain.tracker.AvroTracePoint;
 import com.example.domain.tracker.TracePoint;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.example.util.AvroCodec;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.integration.support.MessageBuilder;
@@ -13,15 +15,15 @@ import org.springframework.stereotype.Component;
 import com.example.domain.tracker.CarActivityDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.stream.Collectors;
+
+@Slf4j
 @Component
 @EnableBinding(ActivitySource.class)
 public class TrackerToKafka {
 	
-	private static final Logger log = LoggerFactory.getLogger(TrackerToKafka.class);
+	private static final String CONTENT_TYPE_AVRO = "binary/avro";
 
-	@Autowired
-	private ObjectMapper mapper;
-	
 	@Autowired
 	private ActivitySource output;
 	
@@ -32,21 +34,33 @@ public class TrackerToKafka {
 				.name("KafkaProducer")
 				.time(System.currentTimeMillis())
 				.build());
-		Message<?> message = buildDefaultMessage(dto);
+		Message<?> message = buildAvroMessage(dto);
 		output.sendTracking().send(message);
 		log.debug("sent to kafka " + message);
 	}
 	
-	
-    private Message<?> buildDefaultMessage(CarActivityDTO dto)
-    {
-    	String carActivity = "";
-    	try {
-    		carActivity = mapper.writeValueAsString(dto);
-    	} catch (Exception e) {}
-    	
-    	return MessageBuilder.withPayload(carActivity)
-			.setHeader(MessageHeaders.CONTENT_TYPE, "application/json")
-			.build();
-    }
+	private Message<?> buildAvroMessage(CarActivityDTO carActivity) {
+		AvroCarActivity avroCar = AvroCarActivity.newBuilder()
+				.setCarID(carActivity.getCarID())
+				.setTime(carActivity.getTime())
+				.setTrace(carActivity.getTrace().stream()
+						.map(tp -> AvroTracePoint.newBuilder()
+								.setName(tp.getName())
+								.setTime(tp.getTime())
+								.build())
+						.collect(Collectors.toList())
+				)
+				.build();
+
+		byte[] payload = null;
+
+		try {
+			payload = new AvroCodec().encode(avroCar);
+		} catch (Exception ex) {}
+
+		return MessageBuilder.withPayload(payload)
+				.setHeader(MessageHeaders.CONTENT_TYPE, CONTENT_TYPE_AVRO)
+				.build();
+
+	}
 }
--- src/main/java/com/example/kafka/tracker/TrackingKafkaToWS.java
+++ src/main/java/com/example/kafka/tracker/TrackingKafkaToWS.java
@@ -2,17 +2,23 @@ package com.example.kafka.tracker;
 
 import javax.inject.Inject;
 
+import com.example.domain.AvroCar;
+import com.example.domain.tracker.AvroCarActivity;
 import com.example.domain.tracker.TracePoint;
+import com.example.util.AvroCodec;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.support.GenericMessage;
 import org.springframework.stereotype.Component;
 
 import com.example.domain.tracker.CarActivityDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.stream.Collectors;
 
 @Component
 @EnableBinding({ActivitySink.class})
@@ -26,14 +32,38 @@ public class TrackingKafkaToWS {
     @Inject
     SimpMessageSendingOperations messagingTemplate;
 	
-    @StreamListener(ActivitySource.TOPIC)
-	public void loggerSink(CarActivityDTO dto) {
-		log.debug("Received from Kafka: " + dto);
-		dto.addTracePoint(TracePoint.builder()
-				.name("KafkaConsumer")
-				.time(System.currentTimeMillis())
-				.build());
-		sendToClients(dto);
+    @StreamListener(ActivitySink.TOPIC)
+	public void loggerSink(GenericMessage<byte[]> message) {
+        log.debug("Received Generic messsage: " + message);
+
+        byte[] payload = message.getPayload();
+
+        AvroCodec codec = new AvroCodec();
+        AvroCarActivity payloadCar = null;
+        try {
+            payloadCar = codec.decode(payload, AvroCarActivity.class);
+        }catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        CarActivityDTO carActivity = CarActivityDTO.builder()
+                .carID(payloadCar.getCarID().toString())
+                .time(payloadCar.getTime().toString())
+                .trace(payloadCar.getTrace().stream()
+                        .map(atp -> TracePoint.builder()
+                                .name(atp.getName().toString())
+                                .time(atp.getTime())
+                                .build())
+                        .collect(Collectors.toList())
+                )
+                .build();
+
+        carActivity.addTracePoint(TracePoint.builder()
+                .name("KafkaConsumer")
+                .time(System.currentTimeMillis())
+                .build());
+
+		sendToClients(carActivity);
 	}
 
 	public void sendToClients(CarActivityDTO dto) {
